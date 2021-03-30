f46b091bf79f6752a0975ec185fa1465fece9690
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2021-03-30 15:55:04 +0530

    FCM foreground complete

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,18 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 const AndroidNotificationChannel channel = AndroidNotificationChannel(
-  'high_importance_channel', // id
-  'High Importance Notifications', // title
-  'This channel is used for important notifications.', // description
-  importance: Importance.max,
-);
+    'high_importance_channel', // id
+    'High Importance Notifications', // name
+    'This channel is used for important notifications.', // description
+    importance: Importance.high);
+
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp();
+
   await flutterLocalNotificationsPlugin
       .resolvePlatformSpecificImplementation<
           AndroidFlutterLocalNotificationsPlugin>()
@@ -55,15 +56,45 @@ class _MainPageState extends State<MainPage> {
   @override
   void initState() {
     super.initState();
-    FirebaseMessaging.onMessage.listen((message) {
-      print('Got message in foreground');
-      print('message is: ${message.data}');
-      if (message.notification != null) {
-        print('messsage also had notificatoins');
-        setState(() {
-          _newNotification = true;
-        });
+
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage message) {
+      if (message != null) {
+        print('initial message read. GHUSA');
       }
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('ghusa in recieved message foredound');
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+
+      // If `onMessage` is triggered with a notification, construct our own
+      // local notification to show to users using the created channel.
+      if (notification != null && android != null) {
+        print('Ghusa in making notification');
+        flutterLocalNotificationsPlugin
+            .show(
+                5,
+                notification.title,
+                notification.body,
+                NotificationDetails(
+                  android: AndroidNotificationDetails(
+                    channel.id,
+                    channel.name,
+                    channel.description,
+                    icon: 'launch_background',
+                    importance: Importance.high,
+                    // other properties...
+                  ),
+                ))
+            .then((value) => print('finish creting notification'));
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
     });
   }
 
